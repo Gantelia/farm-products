d25d847d3ee16fb98c65b6a8d0f3a3c03c51c08d
Gantelia <helengriva@gmail.com>
2023-01-24 13:58:51 +0300

    Уберет отрисовку секции Features при отсутствии данных

--- src/components/blocks/features/features.jsx
+++ src/components/blocks/features/features.jsx
@@ -6,27 +6,23 @@ import Button from '../../ui/button/button.jsx';
 import { StyledFeatures, FeatureList } from './styles';
 
 function Features({ features }) {
-  return (
+  return features?.length && (
     <StyledFeatures>
-      {features?.length && (
-        <>
-          <Title
-            titleLevel={TitleLevel.H2}
-            size={TitleSize.MEDIUM}
-            marginBottom={64}
-          >
-            Почему фермерские продукты лучше?
-          </Title>
-          <FeatureList>
-            {features?.map((feature) => (
-              <li key={feature.id}>
-                <FeatureCard {...feature} />
-              </li>
-            ))}
-          </FeatureList>
-          <Button link={'/buy'}>Купить</Button>
-        </>
-      )}
+      <Title
+        titleLevel={TitleLevel.H2}
+        size={TitleSize.MEDIUM}
+        marginBottom={64}
+      >
+        Почему фермерские продукты лучше?
+      </Title>
+      <FeatureList>
+        {features?.map((feature) => (
+          <li key={feature.id}>
+            <FeatureCard {...feature} />
+          </li>
+        ))}
+      </FeatureList>
+      <Button link={'/buy'}>Купить</Button>
     </StyledFeatures>
   );
 }
